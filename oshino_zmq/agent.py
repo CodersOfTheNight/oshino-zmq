63d3ee368b61778042d0bdba2632c988e1762c27
Šarūnas Navickas <sarunas.navickas@adform.com>
2017-06-21 10:52:26 +0300

    Basic zmq agent

--- oshino_zmq/agent.py
+++ oshino_zmq/agent.py
@@ -1,5 +1,41 @@
+import json
+
+import zmq
+
+from zmq.asyncio import Context
+
 from oshino import Agent
 
 
-class StubAgent(Agent):
-    pass
+class ZmqAgent(Agent):
+
+    @property
+    def bind(self):
+        return self._data.get("bind", None)
+
+    @property
+    def connect(self):
+        return self._data["connect"]
+
+    def parse_logentry(self, json_obj):
+        return json_obj
+
+    async def process(self, event_fn):
+        logger = self.get_logger()
+
+        msg = await self.socket.recv().decode("UTF-8")
+        logger.trace("Received msg: '{0}'".format(msg))
+        json_obj = json.loads(msg)
+        log_obj = self.parse_logentry(json_obj)
+        event_fn(service=self.prefix, **log_obj)
+
+    def on_start(self):
+        self.ctx = Context()
+        self.socket = ctx.socket(zmq.PULL)
+        if self.bind:
+            self.socket.bind(self.bind)
+        else:
+            self.socket.connect(self.connect)
+
+    def on_stop(self):
+        self.socket.close()
