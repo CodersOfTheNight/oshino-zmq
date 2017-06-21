0c5e61d022a96b996ccadac0388ce68245eeed5d
Šarūnas Navickas <sarunas.navickas@adform.com>
2017-06-21 15:10:23 +0300

    Bugfixes for agent

--- oshino_zmq/agent.py
+++ oshino_zmq/agent.py
@@ -1,5 +1,3 @@
-import json
-
 import zmq
 
 from zmq.asyncio import Context
@@ -24,20 +22,25 @@ class ZmqAgent(Agent):
 
     async def process(self, event_fn):
         logger = self.get_logger()
-
-        msg = await self.socket.recv().decode("UTF-8")
-        logger.trace("Received msg: '{0}'".format(msg))
-        json_obj = json.loads(msg)
-        log_obj = self.parse_logentry(json_obj)
-        event_fn(service=self.prefix, **log_obj)
+        if self.socket_active:
+            json_obj = await self.socket.recv_json()
+            logger.trace("Received msg: '{0}'".format(json_obj))
+            log_obj = self.parse_logentry(json_obj)
+            event_fn(service=self.prefix, **log_obj)
+        else:
+            logger.debug("Zmq socket is still waiting for connection")
 
     def on_start(self):
         self.ctx = Context()
         self.socket = ctx.socket(zmq.PULL)
         if self.bind:
             self.socket.bind(self.bind)
+            logger.info("Zmq socket bound on: {0}".format(self.bind))
+            self.socket_active = True
         else:
             self.socket.connect(self.connect)
+            logger.info("Zmq socket connected to: {0}".format(self.connect))
+            self.socket_active = True
 
     def on_stop(self):
         self.socket.close()
