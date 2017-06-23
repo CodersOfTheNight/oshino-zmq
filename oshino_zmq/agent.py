84fdf9888db1205a8b9ed4220838998cded4d935
Šarūnas Navickas <sarunas.navickas@adform.com>
2017-06-23 08:23:14 +0300

    Use non-blocking interface instead

--- oshino_zmq/agent.py
+++ oshino_zmq/agent.py
@@ -1,6 +1,6 @@
 import zmq
 
-from zmq.asyncio import Context, Poller
+from zmq.asyncio import Context
 
 from oshino import Agent
 
@@ -22,17 +22,16 @@ class ZmqAgent(Agent):
 
     async def process(self, event_fn):
         logger = self.get_logger()
-        if self.socket_active and self.poller:
+        if self.socket_active:
             try:
-                events = await self.poller.poll()
+                logger.trace("Trying to read msg")
                 if self.socket in dict(events):
-                    logger.trace("Trying to read msg")
-                    json_obj = await self.socket.recv_json()
+                    json_obj = await self.socket.recv_json(zmq.NOBLOCK)
                     logger.trace("Received msg: '{0}'".format(json_obj))
                     log_obj = self.parse_logentry(json_obj)
                     event_fn(service=self.prefix, **log_obj)
             except:
-                logger.trace("Failed to read msg")
+                logger.trace("No message received")
         else:
             logger.debug("Zmq socket is still waiting for connection")
 
@@ -41,8 +40,6 @@ class ZmqAgent(Agent):
         logger.info("Initializing zMQ context")
         self.ctx = Context()
         self.socket = self.ctx.socket(zmq.PULL)
-        self.poller = Poller()
-        self.poller.register(self.socket, zmq.POLLIN)
         if self.bind:
             self.socket.bind(self.bind)
             logger.info("Zmq socket bound on: {0}".format(self.bind))
