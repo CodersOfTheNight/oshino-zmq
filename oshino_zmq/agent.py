88ff2c6e34f661e482beec4b5621cc5dbdd86c56
Šarūnas Navickas <sarunas.navickas@adform.com>
2017-06-23 08:13:15 +0300

    Use poller for fetching messages

--- oshino_zmq/agent.py
+++ oshino_zmq/agent.py
@@ -1,6 +1,6 @@
 import zmq
 
-from zmq.asyncio import Context
+from zmq.asyncio import Context, Poller
 
 from oshino import Agent
 
@@ -22,13 +22,15 @@ class ZmqAgent(Agent):
 
     async def process(self, event_fn):
         logger = self.get_logger()
-        if self.socket_active:
-            logger.trace("Trying to read msg")
+        if self.socket_active and self.poller:
             try:
-                json_obj = await self.socket.recv_json()
-                logger.trace("Received msg: '{0}'".format(json_obj))
-                log_obj = self.parse_logentry(json_obj)
-                event_fn(service=self.prefix, **log_obj)
+                events = await self.poller.poll()
+                if self.socket in dict(events):
+                    logger.trace("Trying to read msg")
+                    json_obj = await self.socket.recv_json()
+                    logger.trace("Received msg: '{0}'".format(json_obj))
+                    log_obj = self.parse_logentry(json_obj)
+                    event_fn(service=self.prefix, **log_obj)
             except:
                 logger.trace("Failed to read msg")
         else:
@@ -39,6 +41,8 @@ class ZmqAgent(Agent):
         logger.info("Initializing zMQ context")
         self.ctx = Context()
         self.socket = self.ctx.socket(zmq.PULL)
+        self.poller = Poller()
+        self.poller.register(self.socket, zmq.POLLIN)
         if self.bind:
             self.socket.bind(self.bind)
             logger.info("Zmq socket bound on: {0}".format(self.bind))
